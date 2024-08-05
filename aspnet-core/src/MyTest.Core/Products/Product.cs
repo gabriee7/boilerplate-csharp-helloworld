00ad45d0239f844de020d6fdbf1bf8be4effa780
Gabriel Souza <gabrielsouzades@gmail.com>
2024-08-05 10:26:39 -0300

    Product entity and migration

---
+++ aspnet-core/src/MyTest.Core/Products/Product.cs
@@ -0,0 +1,15 @@
+﻿using Abp.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyTest.Products
+{
+    public class Product : Entity<int> 
+    {
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+    }
+}
